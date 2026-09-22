dde44c1741233ff215c81932b9c38c38026cdc25
Kinza Rehman <kinzarehman38@gmail.com>
2026-09-22 18:10:22 -0400

    Javascript Completed

--- Weather_api/main.js
+++ Weather_api/main.js
@@ -1,22 +1,34 @@
 
+//Goal: create a weather app that takes a uses Country and City and gives the weather! 
+//i want the user to input their city and country
+//i want the API to take the city and country and return index[0] temperature for the values 
+//if no values exist i want error to be 'doesn't exist'
+//https://www.weatherapi.com/docs/ 
+//website used for API
+
 document.querySelector('#submit').addEventListener("click", getWeather)
 
 function getWeather () {
     event.preventDefault()
-    
-    const countryInput = document.getElementById("CityCountry").value.toLowerCase()
+    const cityInput = document.getElementById("City").value.toLowerCase()
+    const countryInput = document.getElementById("country").value.toUpperCase()
     console.log(countryInput);
+    console.log(cityInput)
 
+    const fullPlace = cityInput + ", " + countryInput
 
-    const apiUrl =`http://api.openweathermap.org/geo/1.0/direct?q=${countryInput}&appid=52d3da7e875efdb76b73e13d0b543ca0`
+//coordiantes by location name 
+    const apiUrl =`http://api.weatherapi.com/v1/current.json?key={INSERT API KEY HERE}&q=${fullPlace}&aqi=no`
     fetch(apiUrl)
     .then(res => res.json())
     .then(data => {
         console.log(data)
+        document.querySelector(".country").innerHTML ="State: " + data.location.country;
+        document.querySelector(".city").innerHTML = "City: " + data.location.name;
+        document.querySelector(".date").intterHTML ="Date Updated: " + data.current.last_updated;
+        document.querySelector(".temperatureC").innerHTML = "Temp in C: " + data.current.temp_c;
+        document.querySelector(".temperatureF").innerHTML = "Temp in F: " + data.current.temp_f;
+        document.querySelector(".text").innerHTML = "Condition: " + data.current.condition.text;
     })
-
-
-
-
 
 }
