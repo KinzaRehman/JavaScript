4e37a722952087f477225c90fa7cd63c9417656c
Kinza Rehman <kinzarehman38@gmail.com>
2026-09-22 16:02:29 -0400

    adding stuff

--- Weather_api/main.js
+++ Weather_api/main.js
@@ -1,34 +1,22 @@
-//define the metric we need to get
 
-//https://open-meteo.com/en/docs/geocoding-api#api_documentation
-//it can take city, country so concatenate the users input 
-//For example, Paris, France, Los Angeles, California or Los Angeles, CA. 
+document.querySelector('#submit').addEventListener("click", getWeather)
 
-
-//asking the country and city and combining this to match the API stuff
-
-let countryInput = document.getElementById("country")
-let cityInput =document.getElementById("City")
-
-let cityCountry = cityInput +", " + countryInput
-
-const search = document.getElementById("submit")
-search.addEventListener("click", getWeather)
-const theTemperature = document.getElementsByClassName("temperature")
-
-//API link stuff 
-
-const url ="https://geocoding-api.open-meteo.com/v1/search?name=Berlin&count=10&language=en&format=json"
-const apiKey = 
-
-
-
-function getWeather() {
-    const fullArea= cityCountry.value.trim()
-
-    //get geo coding information
-
-    const geourl = 
+function getWeather () {
+    event.preventDefault()
     
-}
+    const countryInput = document.getElementById("CityCountry").value.toLowerCase()
+    console.log(countryInput);
 
+
+    const apiUrl =`http://api.openweathermap.org/geo/1.0/direct?q=${countryInput}&appid=52d3da7e875efdb76b73e13d0b543ca0`
+    fetch(apiUrl)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+    })
+
+
+
+
+
+}
