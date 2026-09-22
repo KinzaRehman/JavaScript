73abe84cf6a4fdf6eb476e3ef3e8ec3f2236084f
Kinza Rehman <kinzarehman38@gmail.com>
2026-09-22 13:44:10 -0400

    JavaScript

--- Weather_api/main.js
+++ Weather_api/main.js
@@ -1,1 +1,26 @@
 //define the metric we need to get
+
+//https://open-meteo.com/en/docs/geocoding-api#api_documentation
+//it can take city, country so concatenate the users input 
+//For example, Paris, France, Los Angeles, California or Los Angeles, CA. 
+
+
+const countryInput = document.getElementById("country")
+const cityInput =document.getElementById("City")
+const search = document.getElementById("submit")
+search.addEventListener("click", getWeather)
+const theTemperature = document.getElementsByClassName("temperature")
+
+
+
+
+function getWeather() {
+    const country = countryInput.value.trim()
+    const city = cityInput.value.trim(); 
+
+    //get geo coding information
+
+    const geourl = 
+    
+}
+
