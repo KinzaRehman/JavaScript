0e7ffd6ca66f417f1153021359f2f51b40890c47
Kinza Rehman <kinzarehman38@gmail.com>
2026-09-22 13:59:52 -0400

    adding jS

--- Weather_api/main.js
+++ Weather_api/main.js
@@ -5,18 +5,26 @@
 //For example, Paris, France, Los Angeles, California or Los Angeles, CA. 
 
 
-const countryInput = document.getElementById("country")
-const cityInput =document.getElementById("City")
+//asking the country and city and combining this to match the API stuff
+
+let countryInput = document.getElementById("country")
+let cityInput =document.getElementById("City")
+
+let cityCountry = cityInput +", " + countryInput
+
 const search = document.getElementById("submit")
 search.addEventListener("click", getWeather)
 const theTemperature = document.getElementsByClassName("temperature")
 
+//API link stuff 
+
+const url ="https://geocoding-api.open-meteo.com/v1/search?name=Berlin&count=10&language=en&format=json"
+const apiKey = 
 
 
 
 function getWeather() {
-    const country = countryInput.value.trim()
-    const city = cityInput.value.trim(); 
+    const fullArea= cityCountry.value.trim()
 
     //get geo coding information
 
