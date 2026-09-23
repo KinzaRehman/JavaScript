e1b2c26f067eb8059db20d943fd959e1a0514e87
Kinza Rehman <kinzarehman38@gmail.com>
2026-09-23 09:12:17 -0400

    Adding CSS

--- Simple-Nasa_api/js/main.js
+++ Simple-Nasa_api/js/main.js
@@ -3,6 +3,7 @@
 
 
 document.querySelector('button').addEventListener('click', getContent);
+document.querySelector('video').style.display = "none"
 
 
 function getContent() {
@@ -12,22 +13,32 @@ function getContent() {
 fetch(apiLink)
     .then(res => res.json())
     .then(data => {
-            console.log(data);      
+
+            console.log(data);   
+            // if (data.media_type ==='image') {
+            //     document.queryselector('img').style.display = "block"
+            //     document.queryselector('video').style.display = "none"
+            // }else if(data.media_type === 'video') {
+            //     document.queryselector('video').style.display = "block"
+            //     document.queryselector('img').style.display = "none"
+            // }   
             document.querySelector('.title').innerHTML = "Title: " + data.title
             document.querySelector('.dates').innerHTML = "The  sky on this date was: " + data.date
             document.querySelector('img').src= data.hdurl
+            document.querySelector('video').src = data.url
             document.querySelector('.explanation').innerHTML = data.explanation;
             if (data.media_type ==='image') {
-                document.querySelector('img').src = data.hdurl;
-            }else {
-                document.querySelector('iframe').src = data.hdurl
+                document.querySelector('img').style.display = "block"
+                document.querySelector('video').style.display = "none"
+            }else if(data.media_type === 'video') {
+                document.querySelector('video').style.display = "block"
+                document.querySelector('img').style.display = "none"
             }
 
         })  
         .catch(err => {
             console.log(`error${err}`)
-        })
-       
+        })      
 }
 
 
