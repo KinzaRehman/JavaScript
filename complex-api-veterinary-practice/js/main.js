cf9929fb7b8ed2e86093dd260152ebada97309dd
Kinza Rehman <kinzarehman38@gmail.com>
2026-09-24 22:53:41 -0400

    making changes

--- complex-api-veterinary-practice/js/main.js
+++ complex-api-veterinary-practice/js/main.js
@@ -29,22 +29,60 @@
 //             console.log(`error ${err}`)
 //         })
 // }
-
-
+/*
+user enters the breed-> hits bloomington api  column breed ->this is then referenced to fda 
+the fda takes the breed .data.results[0].breed -> (potential information output: Vet different animal breed medicine)
+ex: drug[0].manufacturer.name
+*/
+//?pageNumber=1&pageSize=10&$breed=${pet}
 //https://data.bloomington.in.gov/api/v3/views/up5q-gxym/query.json
 
 document.querySelector('#submit').addEventListener("click", getPetCare)
+
 function getPetCare() {
     const pet = document.getElementById("PET").value.toLowerCase()
 
-    const apiUrl = `https://api.fda.gov/animalandveterinary/event.json?limit=5`
+    const mainApiUrl = `https://data.bloomington.in.gov/api/v3/views/up5q-gxym/query.json`
 
-    fetch(apiUrl)
-        .then(res => res.json())
+    
+    fetch(mainApiUrl, {method:"POST", body:JSON.stringify({
+        "query": `SELECT * WHERE lower(breed) like lower('%${pet}%')`,
+        "page": {
+            "pageNumber": 1,
+            "pageSize": 100
+        },
+        "includeSynthetic": false
+     })} )
+        .then(result => result.json())
         .then(data => {
             console.log(data)
-        })
-        .catch(err => {
-            console.log("ERROR:", err)
-        })
+            document.querySelector(".one").innerHTML = "Youre searching for  medication for " + data[0].breed
+
+
+            let newPetInfo = data[0].breed; 
+
+            const apiUrl = `https://api.fda.gov/animalandveterinary/event.json?limit=5&search=animal.breed.breed_component:${newPetInfo}`
+
+                fetch(apiUrl)
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data)
+                        document.querySelector(".main").innerHTML = data.meta.disclaimer
+                        document.querySelector(".two").innerHTML = data.results[0].animal.gender
+
+                        document.querySelector(".two").innerHTML = data.results[0].drug[0].manufacturer.name
+                        document.querySelector(".three").innerHTML = data.results[0].drug[0].manufacturer.registration_number
+
+                    })
+                    .catch(err => {
+                        console.log("ERROR:", err)
+                    })
+
+                    })
+                    .catch(err => {
+                        console.log("Error", err)
+
+                        
+                   })
+
 }
